95ba2637727089df9899f38352d7b335e45c0456
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-07-18 15:55:06 -0300

    Validation and transform with zod

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -1,14 +1,27 @@
 import { Button, Checkbox, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
 import { Container, Header } from "../styles";
-import { IntervalBox, IntervalDay, IntervalInputs, IntervalItem, IntervalsContainter } from "./styles";
+import { FormError, IntervalBox, IntervalDay, IntervalInputs, IntervalItem, IntervalsContainter } from "./styles";
 import { ArrowRight } from "phosphor-react";
 import { useForm, useFieldArray, Controller } from 'react-hook-form'
 import { z } from "zod";
 import { getWeekDays } from "@/utils/get-week-days";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const timeIntervalsFormSchema = z.object({
-
+    intervals: z.array(z.object({
+        weekDay: z.number().min(0).max(6),
+        enabled: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
+    }))
+        .length(7)
+        .transform(intervals => intervals.filter(interval => interval.enabled))
+        .refine(intervals => intervals.length > 0, {
+            message: 'Você precisa selecionar pelo menos um dia da semana!'
+        })
 })
+
+type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
 
 export default function TimeIntervals() {
     const {
@@ -18,6 +31,7 @@ export default function TimeIntervals() {
         watch,
         formState: { isSubmitting, errors }
     } = useForm({
+        resolver: zodResolver(timeIntervalsFormSchema),
         defaultValues: {
             intervals: [
                 { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
@@ -40,8 +54,8 @@ export default function TimeIntervals() {
 
     const intervals = watch('intervals')
 
-    async function handleSetTimeIntervals() {
-
+    async function handleSetTimeIntervals(data: TimeIntervalsFormData) {
+        console.log(data)
     }
 
     return (
@@ -96,7 +110,11 @@ export default function TimeIntervals() {
                     })}
                 </IntervalsContainter>
 
-                <Button type="submit">
+                {errors.intervals && (
+                    <FormError size="sm">{errors.intervals.message}</FormError>
+                )}
+
+                <Button type="submit" disabled={isSubmitting}>
                     Próximo passo
                     <ArrowRight />
                 </Button>
