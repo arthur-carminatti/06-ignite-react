8ddc90eb6f52bade9f95742f7c7b97216cecbee8
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-07-26 17:25:00 -0300

    Browsing the months

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,9 +1,7 @@
 import { CalendarStep } from "./CalendarStep";
-import { ConfirmStep } from "./ConfirmStep";
 
 export function ScheduleForm() {
     return (
-        // <CalendarStep></CalendarStep>
-        <ConfirmStep></ConfirmStep>
+        <CalendarStep></CalendarStep>
     )
 }
