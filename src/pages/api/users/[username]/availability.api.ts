7b678c5fa629dba72e0d49b2cc3e5688b0332ebe
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-07-27 16:17:38 -0300

    Loading times

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -50,10 +50,30 @@ export default async function handle(
     const startHour = time_start_in_minutes / 60
     const endHour = time_end_in_minutes / 60
 
-    const possibleTimes = Array.from({ length: endHour - startHour })
-        .map((_, i) => {
+    const possibleTimes = Array.from({ length: endHour - startHour }).map(
+        (_, i) => {
             return startHour + i
-        })
+        }
+    )
 
-    return res.json({ possibleTimes })
+    const blockedTimes = await prisma.scheduling.findMany({
+        select: {
+            date: true
+        },
+        where: {
+            user_id: user.id,
+            date: {
+                gte: referenceDate.set('hour', startHour).toDate(),
+                lte: referenceDate.set('hour', endHour).toDate(),
+            }
+        }
+    })
+
+    const availableTimes = possibleTimes.filter((time) => {
+        return !blockedTimes.some(
+            (blockedTime) => blockedTime.date.getHours() === time
+        )
+    })
+
+    return res.json({ possibleTimes, availableTimes })
 }
